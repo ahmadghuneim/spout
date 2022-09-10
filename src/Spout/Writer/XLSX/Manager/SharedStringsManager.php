30054c17155d839f49347e1128cd637585481837
Ahmad Ghuneim <ahmad.ghuneim@hyperpay.com>
2022-09-11 01:25:35 +0300

    Spout

--- src/Spout/Writer/XLSX/Manager/SharedStringsManager.php
+++ src/Spout/Writer/XLSX/Manager/SharedStringsManager.php
@@ -4,7 +4,7 @@ namespace Box\Spout\Writer\XLSX\Manager;
 
 use Box\Spout\Common\Exception\IOException;
 use Box\Spout\Common\Helper\Escaper;
-
+use Illuminate\Support\Facades\Log;
 /**
  * Class SharedStringsManager
  * This class provides functions to write shared strings
@@ -22,8 +22,8 @@ EOD;
      * This number must be really big so that the no generated file will have more strings than that.
      * If the strings number goes above, characters will be overwritten in an unwanted way and will corrupt the file.
      */
-//    public const DEFAULT_STRINGS_COUNT_PART = 'count="9999999999999" uniqueCount="9999999999999"';
-    public const DEFAULT_STRINGS_COUNT_PART = 'count="9999999999999999999999999999999999" uniqueCount="9999999999999999999999999999999999"';
+    public const DEFAULT_STRINGS_COUNT_PART = 'count="9999999999999999" uniqueCount="9999999999999999"';
+
 
     /** @var resource Pointer to the sharedStrings.xml file */
     protected $sharedStringsFilePointer;
@@ -48,6 +48,8 @@ EOD;
         // the headers is split into different parts so that we can fseek and put in the correct count and uniqueCount later
         $header = self::SHARED_STRINGS_XML_FILE_FIRST_PART_HEADER . ' ' . self::DEFAULT_STRINGS_COUNT_PART . '>';
         \fwrite($this->sharedStringsFilePointer, $header);
+        Log::alert('header');
+        Log::alert($header);
 
         $this->stringsEscaper = $stringsEscaper;
     }
